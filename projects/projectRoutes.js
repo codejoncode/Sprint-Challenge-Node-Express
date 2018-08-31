c671bf5d5392eb1010ca0471cbede20ba81024b6
codejoncode <jonathanjholloway@gmail.com>
2018-08-31 10:39:59 -0500

    posting a project with the requiredfields/rules for fields

--- projects/projectRoutes.js
+++ projects/projectRoutes.js
@@ -1,19 +1,60 @@
-const express = require("express")
-const projectDb = require('../data/helpers/projectModel.js')
-const projectRouter = express.Router(); 
+const express = require("express");
+const projectDb = require("../data/helpers/projectModel.js");
+const projectRouter = express.Router();
 
-projectRouter.get('/', (req, res) => {
-    projectDb.get()
+//MiddleWare
+
+function bodyChecker(req, res, next) {
+  const { name, description, completed } = req.body;
+  if (name.length > 128) {
+    res
+      .status(500)
+      .json({
+        message:
+          `${name.length} <<name is too long needs to be less than or equal to 128 characters`
+      });
+  }
+  if (typeof name !== "string") {
+    res.status(500).json({ message: "name must be a string" });
+  }
+  if (typeof description !== "string") {
+    res.status(500).json({ message: "description must be a string" });
+  }
+  if (completed) {
+    if (typeof completed !== "boolean") {
+      res
+        .status(500)
+        .json({
+          message:
+            "The completed field is not required but must be type boolean true or false"
+        });
+    }
+  }
+  req.posting = completed ? {name, description, completed} : {name, description}
+  next()
+}
+
+//Routes
+projectRouter.get("/", (req, res) => {
+  projectDb
+    .get()
     .then(projects => {
-        res.status(200).json(projects)
+      res.status(200).json(projects);
     })
     .catch(error => {
-        res.status(500).json({error, message: "Unable to obtain projects"})
+      res.status(500).json({ error, message: "Unable to obtain projects" });
+    });
+});
+
+projectRouter.post("/", bodyChecker, (req, res) => {
+  projectDb
+    .insert(req.posting)
+    .then(posted => {
+      res.status(201).json(req.posting)
     })
-})
+    .catch(error => {
+      res.status(500).json({error, message: "unable to save new project"})
+    })
+});
 
-
-
-
-
-module.exports = projectRouter; 
+module.exports = projectRouter;

78ae24a4754c03e46648bbf817d0610cc1285d16
codejoncode <jonathanjholloway@gmail.com>
2018-08-31 11:04:03 -0500

    able to successfully get the actions

--- actions/actionRoutes.js
+++ actions/actionRoutes.js
@@ -5,5 +5,16 @@ const actionRouter = express.Router()
 //MiddleWare
 
 //Routes 
+actionRouter.get('/', (req,res) => {
+  actionDb
+    .get()
+    .then(actions => {
+      res.status(200).json(actions)
+    })
+    .catch(error => {
+      res.status(500).json({error, message: "Unable to obtain actions"})
+    })
+})
+
 
 module.exports = actionRouter; 
